e4f8e17486ae3e3ee4fa3cac2407c469591306f3
Jonathan Tsui <tsui.jonathan@gmail.com>
2024-12-12 22:07:22 +0100

    added updated cleaned csv and queries

--- sql/veronicas_queries.sql
+++ sql/veronicas_queries.sql
@@ -54,10 +54,94 @@ SELECT *
 FROM Sales
 WHERE gross_sales < 100 and is_store_open = 1;
 
--- Sales on Holidays
+-- Sales on Holidays and When the Store is Open
 SELECT
 	s.date,
-    s.net_sales,
-    h.is_holiday
+    SUM(s.net_sales) AS holiday_sales,
+    s.is_store_open,
+    h.is_holiday,
+    h.holiday_name
+FROM Sales s
+JOIN Holiday h ON s.date = h.date
+WHERE h.is_holiday = 1 and s.is_store_open = 1
+GROUP BY s.date, s.is_store_open, h.is_holiday, h.holiday_name
+ORDER BY holiday_sales DESC;
+
+-- AVG sales on high temperature days
+SELECT s.date, s.net_sales, w.mean_temp_c
+FROM Sales s
+JOIN Weather w ON s.date = w.date
+ORDER BY s.net_sales DESC
+LIMIT 10;
+
+-- Weekly sales
+SELECT
+    YEAR(s.date) AS year,
+    WEEK(s.date, 1) AS week_number,  -- '1' indicates weeks start on Monday
+    SUM(s.net_sales) AS weekly_sales
+FROM Sales s
+GROUP BY YEAR(s.date), WEEK(s.date, 1)
+ORDER BY year, week_number;
+
+-- Sales by Payment Method
+SELECT 
+	SUM(cash) AS total_cash,
+    SUM(card) AS total_card,
+    SUM(gift_card) AS total_gift_card
+FROM Payments;
+
+-- Sales on snow days
+SELECT s.net_sales, s.date, s.is_store_open, w.total_snow_mm
+FROM Sales s
+JOIN Weather w ON s.date = w.date
+WHERE w.total_snow_mm > 0 AND s.is_store_open = 1
+ORDER BY s.net_sales; 
     
+-- Average tip amount
+SELECT AVG(tip)
+FROM Sales;
+
+-- Gift Card Sales by Month
+SELECT 
+	DATE_FORMAT (date, '%y-%m') AS month,
+    SUM(gift_card_sales)
+FROM Sales
+GROUP BY month
+ORDER BY month;
     
+-- Gift card sales 1 or 2 days before a holiday
+SELECT
+	s.date,
+    s.gift_card_sales
+FROM Sales s
+JOIN Holiday h ON s.date = h.date
+WHERE s.gift_card_sales > 0 AND h.is_holiday_prev_1 = 1 OR h.is_holiday_prev_2 = 1
+ORDER BY s.date;
+
+-- Total fees by month
+SELECT
+	DATE_FORMAT (date, '%y-%m') AS month,
+    SUM(fees)
+FROM Payments
+GROUP BY month
+ORDER BY month;
+
+-- Deleted all rows before 2023-02-01 and all rows after 2024-11-30 as those months are not complete
+DELETE FROM Holiday
+WHERE date < '2023-02-01' OR date > '2024-11-30';
+
+DELETE FROM Sales
+WHERE date < '2023-02-01' OR date > '2024-11-30';
+
+DELETE FROM Weather
+WHERE date < '2023-02-01' OR date > '2024-11-30';
+
+DELETE FROM Payments
+WHERE date < '2023-02-01' OR date > '2024-11-30';
+
+DELETE FROM Dates
+WHERE date < '2023-02-01' OR date > '2024-11-30';
+
+
+
+
